0dd9dbd006b92cbe553e10d9a12ecba061d99b56
Mark G <markghaida@gmail.com>
2020-11-05 21:55:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,6 +11,7 @@ def game_hash
                             :steals => 3, 
                             :blocks => 1, 
                             :slam_dunks => 1},
+                           { :player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7 },
                            
     :away => {}
   }
