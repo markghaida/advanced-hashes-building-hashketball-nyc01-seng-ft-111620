ba238a29c56f3684b64f3cb9545c5d80f9cc6316
Mark G <markghaida@gmail.com>
2020-11-05 22:06:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
 def game_hash 
   {
     :home => {:team_name => "Brooklyn Nets",
-              :color => ["Black","White"],
+              :colors => ["Black","White"],
               :players => [{:player_name => "Alan Anderson",                :number => 0, 
                             :shoe => 16, 
                             :points => 22, 
@@ -17,7 +17,7 @@ def game_hash
                             { :player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1 }]},
     :away => {
               :team_name => "Charlotte Hornets",
-              :color => ["Turquoise","Purple"],
+              :colors => ["Turquoise","Purple"],
               :players => [
       { :player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2 }, 
       { :player_name => "Bismack Biyombo", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10 }, 
